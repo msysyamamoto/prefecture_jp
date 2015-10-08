1f54eaba597879790b149342584d0f62a9550c3a
ymmtmsys <m2yamamoto@gmail.com>
2015-10-08 20:40:36 +0900

    Add a test for all/0

--- test/prefecture_jp_test.exs
+++ test/prefecture_jp_test.exs
@@ -2,7 +2,7 @@ defmodule PrefectureJpTest do
   use ExUnit.Case
   doctest PrefectureJp
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "return 47 elements" do
+      assert Enum.count(PrefectureJp.all) == 47
   end
 end
