3185137f291c2e79eb21701518c26cf9e3170ebc
Masayasu Yamamoto <m2yamamoto@gmail.com>
2016-02-18 23:02:49 +0900

    Bump up version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PrefectureJp.Mixfile do
 
   def project do
     [app: :prefecture_jp,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1",
      description: description,
      package: package,
