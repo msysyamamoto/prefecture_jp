0567008193000a44433e1ed4d7d99fe69f94bf4e
Masayasu Yamamoto <m2yamamoto@gmail.com>
2016-09-24 23:17:29 +0900

    version up

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PrefectureJp.Mixfile do
 
   def project do
     [app: :prefecture_jp,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.1",
      description: description(),
      package: package(),
